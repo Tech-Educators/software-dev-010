428bfb33132a8dfe4a4a2d26d6664f3847167bcc
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-05-09 16:02:30 +0100

    demo

---
+++ week01/day03/primatives/app.js
@@ -0,0 +1,74 @@
+// const is forever.
+
+// they cannot be changed
+const myFavNumber = 42;
+// myFavNumber = 52;
+// console.log(myFavNumber)
+
+// let allows us to re-assign things
+let myName = 'Sam'
+console.log(myName)
+myName = 'Samantha'
+console.log(myName)
+
+// DATA TYPE: STRING
+let darcy = 'Charlie'
+let numberAsAString = '42'
+
+console.log(darcy + numberAsAString)
+// '' - single quotes
+// "" - double quotes
+// `` - backticks. 
+
+console.log(`This is a string literal ${darcy}`)
+console.log(`This is a string literal darcy`)
+
+// DATA TYPES: Numbers
+
+let coolNumber = 42;
+let coolerNumber = 23421
+
+console.log(coolNumber + coolerNumber)
+
+// let myDog = Darcy
+
+// DATA TYPE: Boolean.
+// true / false
+
+let theTruth = true;
+let theFalse = false;
+
+// a single = sign is the assignment operator 
+// a double == sign is an equality operator
+// a triple === sign is a strict equality operator. (We check both value and data type.)
+
+// we also learned some other operators
+
+// 5 < 7 -> true
+// 5 > 2 -> true
+// 6 >= 6 -> true
+// 7 <=  7 -> true
+
+
+// console.log(myDog)
+
+let number1 = 10
+let number2 = 20
+
+let myVar = null
+
+console.log(number1 < number2)
+
+const nummber1 = 'mmm'
+console.log(nummber1)
+
+
+console.log()
+let 
+const
+
+// three data types
+
+// strings = ''
+// numbers = they're numbers = 52
+// booleans = true/false values 
