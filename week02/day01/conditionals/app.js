26b3aa73f1af26fbe1899d73e68a5cc1fff83497
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-05-13 14:48:14 +0100

    conditionals demo

---
+++ week02/day01/conditionals/app.js
@@ -0,0 +1,49 @@
+
+// if somethingIsTRUE - do the code in the {}'s
+
+if (5 > 4) {
+    console.log('5 is greater than 4')
+}
+
+console.log('hello!')
+
+
+// video game character creation.
+let age = 19;
+
+if (age < 13) {
+    console.log('You are a sidekick!')
+} else if (age > 13 && age <= 18) {
+    console.log(`You're the hero!`)
+} else {
+    console.log('You are a wise mentor.')
+}
+
+
+// boolean operators
+
+// && - AND - both expressions need to be true. 
+// ! - NOT - 
+// || - OR - Checks if one expression is true.
+
+
+// === - equality operator - Are things equal to eachother
+// greater than/less than - <  > - Check if numbers are bigger or smaller then eachother.
+
+
+let cupsOfCoffee = 5;
+
+if (cupsOfCoffee <= 5) {
+    console.log('Im sleepy 😴')
+}
+
+if (cupsOfCoffee > 4) {
+    console.log('Thats a lot of coffee ... are you going to sleep.')
+} else {
+    console.log(`You've had coffee!`)
+}
+
+if (cupsOfCoffee == 5) {
+    console.log('Thats 5 cups!')
+}
+
