417accd0e6d26d2b430690cdede07ba0b534e63e
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-05-15 14:50:56 +0100

    objects demo

---
+++ week02/objects-intro/app.js
@@ -0,0 +1,38 @@
+// Objects are a collection of properties and values about a thing.
+
+let myArray = ['fox', 'bird', 'fish']
+
+let coolAnimal = {
+    species: 'Pangolin',
+    activeHours: 'nocturnal',
+    habitat: 'Forests',
+    rating: 5,
+    cute: true,
+    names: ['barry', 'tom'],
+    // environment: {
+    //     name: 'Everwoods glades',
+    //     tres: ['something like this']
+    // }
+}
+
+console.log(coolAnimal)
+
+// how to log one property to the console.
+document.getElementById
+
+console.log(coolAnimal.species)
+console.log(coolAnimal.habitat)
+console.log(coolAnimal.cute)
+
+
+let myHobbies = {
+    type: 'Gaming',
+    location: 'Home',
+    rating: 3
+}
+
+console.log(myHobbies)
+
+myHobbies.controller = 'gamepad'
+
+console.log(myHobbies)
