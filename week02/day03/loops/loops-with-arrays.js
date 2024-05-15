b96252b0619a1f301c45c1b44c4567a529034c1a
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-05-15 13:17:04 +0100

    add loops demo

---
+++ week02/day03/loops/loops-with-arrays.js
@@ -0,0 +1,35 @@
+let hobbieContainer = document.getElementById('hobbies-container') // null
+// 1. fetch data
+// 2. and array of objects
+// 3. loop through that array and use DOM manipulation to display the data.
+
+
+let hobbies = ['Fighting Games', 'Films', 'Programming', 'Baldurs Gate 3', 'wasting time on my phone', 'so many things', 'wow!']
+
+// console.log(hobbies[0])
+// console.log(hobbies[1])
+// console.log(hobbies[2])
+
+// hobbies.length = 3
+
+for (let i = 0; i < hobbies.length; i++) {
+    // create element (h3)
+    let h3 = document.createElement('h3')  // <h3></h3>
+    // set the content of that h3 = hobbies[i]
+    h3.innerText = hobbies[i] // <h3>Fighting games</h3>
+    // append that h3 to my hobbies container
+    hobbieContainer.appendChild(h3) // to the hobbiesContainer, append as a child the h3 we just made.
+    console.log(hobbies[i])
+}
+
+// 0
+// 1
+// 2
+
+
+// When we see this in our console: Uncaught TypeError: Cannot read properties of null (reading 'appendChild')
+
+// there are a few possible reaons: 
+// forgot to link javascript?
+// forgot to put a 'defer' attribute in our script tag.
+// or we made a typo when selecting the element (curse those 's''s)
--- week02/day03/loops/loops.js
+++ week02/day03/loops/loops.js
@@ -33,3 +33,69 @@ let five = 3
 
 3 == 3
 '3' === '5' // false
+
+// write a loop that counts down
+// when the count is 0, print to the console 'Happy new year 🎉' 
+
+
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+// 'Happy new year 🎉!' 
+
+// looping 'backwards' 
+
+for (let i = 5; i >= 0; i--) {
+    
+    console.log(`${i}...`)
+
+    if (i === 0) {
+        console.log('Happy new year 🎉!')
+    }
+
+    hello()
+}
+
+function hello() {
+    // console.log(test)
+}
+
+
+console.log('five plus five is' + ('some'))
+console.log(`five plus five is ${5 + 5}`)
+
+
+// function  - I need some code that 'does one particular thing' that I can call and reuse anywhere
+// let x = 5 - i need to store data that I can use in multiple places
+// for - I need to do some code over and over again a set amount of times
+// if - 'I might need something depending on some condition.'\
+
+
+
+// While loops are a hybrid between if statements and for loops.
+
+if (5 > 4) {
+    console.log('5 is less than 4.')
+}
+
+// let counter = 0;
+
+// while (counter < 10) {
+//     console.log('Whoo')
+//     counter++
+// }
+
+// let userLoggedIn = true
+// while (!userLoggedIn) {
+//     console.log('please sign in')
+// }
+
+
+let randomNumber = 0;
+
+while (randomNumber != 5) {
+    randomNumber = prompt('Pick a random number between 0 & 5')
+}
