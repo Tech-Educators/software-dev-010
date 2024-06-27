34f06259f7e78b623cbb8acacc1b66318fa98b2b
Sam Louloudis <samanthaleelouloudis@gmail.com>
2024-06-27 10:49:05 +0100

    fonts

---
+++ week08/pets-app/src/app/add-pet/page.js
@@ -0,0 +1,68 @@
+import { connect } from "@/utils/connect"
+import { revalidatePath } from "next/cache"
+import { redirect } from "next/navigation"
+
+export default function AddPet() {
+
+    async function handleAddPet(formData) {
+        "use server"
+        console.log(formData)
+    //    const {pet_name, type, breed, habitat, img_url, bio} = formData
+        const pet_name = formData.get('pet_name')
+        const type = formData.get('type')
+        const breed = formData.get('breed')
+        const habitat = formData.get('habitat')
+        const img_url = formData.get('img_url')
+        const bio = formData.get('bio')
+
+        
+
+        const db = connect()
+        // you write the insert statement
+        db.query(`INSERT INTO 
+                  pets (pet_name, type, breed, habitat, img_url, bio) 
+                  VALUES 
+                  ($1, $2, $3, $4, $5, $6)`, 
+                  [
+                    pet_name, type, breed, habitat, img_url, bio
+                  ]
+                )
+
+       console.log(pet_name, type, habitat, img_url, bio)
+       // make sure the database and the data on the home route are the same
+       revalidatePath('/')
+       // redirect the user back to the home route.
+       redirect('/')
+    }
+
+    return (
+        <div>
+            <h1>Add a new pet:</h1>
+            <form action={handleAddPet} className="flex flex-col text-black">
+                <label htmlFor='pet_name'>Enter your pets name:</label>
+                <input placeholder="pet name" name="pet_name" id='pet_name'></input>
+
+                <label htmlFor='type'>Enter your pets type</label>
+                <input placeholder="type..." name="type" id="type"></input>
+
+                <label htmlFor='breed'>Enter your pets breed:</label>
+                <input placeholder="breed..." name="breed" id='breed'></input>
+
+                <label htmlFor='habitat'>Enter your pets habitat:</label>
+                <input placeholder="habitat..." name="habitat" id='habitat'></input>
+
+                <label htmlFor='img_url'>Enter your pets photograph:</label>
+                <input placeholder="URL" name="img_url" id='img_url' type="url"></input>
+
+                <label htmlFor='bio'>Enter your pets bio:</label>
+                <input placeholder="bio" name="bio" id='bio'></input>
+
+                <button type="submit" className="text-white">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+/*
+| id | pet_name | type | breed | habitat | img_url| bio                                                        
+*/
---
+++ week08/pets-app/src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-rose-600 p-4 mb-2.5">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+          <Link href="/">Pets Showcase</Link>
+        </div>
+        <div className="space-x-4">
+          <Link href="/">Home</Link>
+          <Link href="/add-pet">Add a new pet</Link>
+          <Link href="/search">Search</Link>
+          <Link href="/about">About</Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
